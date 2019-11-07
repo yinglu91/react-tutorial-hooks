a8c017b5da00ea78c4d888a0c0ad18ea09e1b491
ylu <ylu@ert.com>
2019-11-07 17:44:54 -0500

    first commit

---
+++ src/components/Table.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+}
+
+const TableBody = () => {
+    return (
+     <tbody>
+        <tr>
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Bouncer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+          <td>Bartender</td>
+        </tr>
+      </tbody>
+    );
+  }
+
+const Table = () => {
+  return (
+    <table>
+        <TableHeader />
+        <TableBody />
+  </table>
+  );
+}
+
+export default Table;
